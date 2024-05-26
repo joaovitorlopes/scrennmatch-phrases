3246e76759c72a74b3473b92adf1f843522257ab
joaovitorlopes <jvlaaa+github@gmail.com>
2024-05-25 20:12:06 -0400

    initial commit

---
+++ src/main/java/joaovitorlopes/com/github/scrennmatch_phrases/ScrennmatchPhrasesApplication.java
@@ -0,0 +1,13 @@
+package joaovitorlopes.com.github.scrennmatch_phrases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrennmatchPhrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrennmatchPhrasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/joaovitorlopes/com/github/scrennmatch_phrases/ScrennmatchPhrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package joaovitorlopes.com.github.scrennmatch_phrases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrennmatchPhrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
